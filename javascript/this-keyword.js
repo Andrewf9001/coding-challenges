4cc2260bc7537644c7abebe1bca0acd62217c08c
AndrewMPFQ <andrewf9001@gmail.com>
2020-09-04 16:54:01 -0600

    added in the start of the full React project

--- javascript/this-keyword.js
+++ javascript/this-keyword.js
@@ -16,3 +16,36 @@ var warrior = {
 };
 
 console.log(warrior.healthRemaining());
+
+const student = {
+  name: "Andrew",
+  age: 29,
+  renderStudent: function () {
+    console.log(this.name, this.age);
+  },
+};
+
+student.renderSTudent = student.renderStudent.bind(student); //will return a new function with a new value that always points to the student
+
+const activities = {
+  render: student.renderStudent,
+};
+
+student.render();
+
+class Student {
+  constructor() {
+    this.name = "Andrew";
+    this.age = 29;
+  }
+
+  addAttribute() {
+    this.test = "Success!";
+  }
+
+  render() {
+    console.log(this.name, this.age);
+  }
+}
+
+const andrew = new Student();
