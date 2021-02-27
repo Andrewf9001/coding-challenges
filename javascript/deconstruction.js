a44c5d730dee951a18852d13cf1674285080beaa
AndrewMPFQ <andrewf9001@gmail.com>
2021-02-27 15:50:35 -0700

    variable deconstruction

---
+++ javascript/deconstruction.js
@@ -0,0 +1,11 @@
+someList = {
+  mazda: "rx7",
+  toyota: ["ae86", "supra"],
+  honda: "accord",
+};
+
+const { mazda, toyota, honda } = someList;
+
+console.log(mazda);
+console.log(toyota[1]);
+console.log(honda);
