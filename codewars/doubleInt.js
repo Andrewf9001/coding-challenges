b0fd0a72fa4f9f0033d0155d35df247ba0dc3abb
Andrewf9001 <andrewf9001@gmail.com>
2024-08-05 15:52:19 -0600

    double integer codewars

---
+++ codewars/doubleInt.js
@@ -0,0 +1,11 @@
+function doubleInteger(i) {
+  return i + i;
+}
+
+// const Test = require('@codewars/test-compat');
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(doubleInteger(2), 4);
+//   });
+// });
