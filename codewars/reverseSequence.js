df37a5474b7c3d7f858065c6b8988c5268feb6f6
Andrewf9001 <andrewf9001@gmail.com>
2024-08-06 16:29:06 -0600

    reverse sequence codewars

---
+++ codewars/reverseSequence.js
@@ -0,0 +1,25 @@
+const reverseSeq = n => {
+  const numsArray = []
+  
+  for (let i = 0; i < n; i++)
+    numsArray.push(i + 1)
+  
+  return numsArray.reverse()
+};
+
+// Instructions:
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+
+// Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("reverseSeq", function() {
+//   it("Sample Test", function() {
+//     assert.deepEqual(reverseSeq(5), [5, 4, 3, 2, 1]);
+//   });
+// });
