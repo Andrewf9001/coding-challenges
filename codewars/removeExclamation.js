8429c47e0486b503f830713a5b0e43a85444b94a
Andrewf9001 <andrewf9001@gmail.com>
2024-08-09 13:51:29 -0600

    remove exclamation codewars

---
+++ codewars/removeExclamation.js
@@ -0,0 +1,16 @@
+const removeExclamationMarks = (str) => str.replaceAll("!", "");
+
+// Instructions:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+// Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.strictEqual(removeExclamationMarks("Hello World!"), "Hello World");
+//   });
+// });
