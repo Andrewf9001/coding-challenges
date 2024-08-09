0e0fe352c1267156c8ff94e2cfe382a0c61b0393
Andrewf9001 <andrewf9001@gmail.com>
2024-08-09 13:40:16 -0600

    toUpperCase codewars

---
+++ codewars/toUpperCase.js
@@ -0,0 +1,16 @@
+const makeUpperCase = (str) => str.toUpperCase();
+
+// Instructions:
+// Write a function which converts the input string to uppercase.
+
+// Tests:
+// const { assert } = require('chai');
+
+// describe("Basic Tests", function(){
+//   it("should pass the basic tests", function(){
+//     assert.strictEqual(makeUpperCase(""),           "");
+//     assert.strictEqual(makeUpperCase("hello"), "HELLO");
+//     assert.strictEqual(makeUpperCase("Hello"), "HELLO");
+//     assert.strictEqual(makeUpperCase("HELLO"), "HELLO");
+//   });
+// });
