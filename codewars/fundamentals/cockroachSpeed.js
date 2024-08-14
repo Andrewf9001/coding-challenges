f19ba5da45857efe7a5a561beeef4d944d247edc
Andrewf9001 <andrewf9001@gmail.com>
2024-08-14 16:35:57 -0600

    cock roach speed codewars

---
+++ codewars/fundamentals/cockroachSpeed.js
@@ -0,0 +1,28 @@
+const cockroachSpeed = (s) => {
+  const cmps = 27.777778
+  
+  if (s === 0) return 0
+  else return Math.floor(cmps * s)
+}
+
+// Instructions:
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+// For example:
+
+// 1.08 --> 30
+// Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+
+// Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic Tests", function() {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(cockroachSpeed(1.08), 30);
+//     assert.strictEqual(cockroachSpeed(1.09), 30);
+//     assert.strictEqual(cockroachSpeed(0), 0);
+//   });
+// });
