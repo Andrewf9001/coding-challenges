fec188db3387f95848d03502d85a79b069f5b664
Andrewf9001 <andrewf9001@gmail.com>
2024-08-18 10:12:18 -0600

    bool to word codewars:

---
+++ codewars/fundamentals/boolToWord.js
@@ -0,0 +1,15 @@
+const boolToWord = ( bool ) => bool ? "Yes" : "No"
+
+// Instructions:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+// Tests:
+// const { assert } = require("chai")
+
+// describe("Basic tests", () => {
+//   it("Testing for basic tests", () => {
+//     assert.strictEqual(boolToWord(true), 'Yes')
+//     assert.strictEqual(boolToWord(false), 'No')
+//     });
+//   });
