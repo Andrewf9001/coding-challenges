4225fc9355bc445c089245b87adbf5930792de6d
Andrewf9001 <andrewf9001@gmail.com>
2024-08-19 16:03:52 -0600

    power of two codewars

---
+++ codewars/fundamentals/powerOfTwo.js
@@ -0,0 +1,17 @@
+const powersOfTwo = (n) => {
+  const numsArray = []
+
+  for (let i = 0; i <= n; i++) {
+    numsArray.push(i)
+  }
+
+  return numsArray.map((num) => Math.pow(2, num));
+}
+
+// Instructions:
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
