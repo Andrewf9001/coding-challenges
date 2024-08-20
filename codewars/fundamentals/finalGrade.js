7bbab6e4c142ae534e05dcff6ba858516a353b7e
Andrewf9001 <andrewf9001@gmail.com>
2024-08-20 16:25:32 -0600

    added submission

--- codewars/fundamentals/finalGrade.js
+++ codewars/fundamentals/finalGrade.js
@@ -42,3 +42,13 @@ const finalGrade = (exam, projects) => {
 //     assert.strictEqual(finalGrade(85, 5), 90);
 //   });
 // });
+
+// Submission:
+// function finalGrade (exam, projects) {
+//   switch (true) {
+//     case exam > 90 || projects > 10: return 100;
+//     case exam > 75 && projects >= 5: return 90;
+//     case exam > 50 && projects >= 2: return 75;
+//     default: return 0;
+//   }
+// }
