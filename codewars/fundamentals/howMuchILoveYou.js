2cc2cd3d9d56479cd7a0811050a1e6241f1bbfbf
Andrewf9001 <andrewf9001@gmail.com>
2024-08-25 12:02:24 -0600

    added submission

--- codewars/fundamentals/howMuchILoveYou.js
+++ codewars/fundamentals/howMuchILoveYou.js
@@ -33,3 +33,16 @@ const howMuchILoveYou = (nbPetals) => {
 //     assert.strictEqual(howMuchILoveYou(6),"not at all")
 //   });
 // })
+
+
+// Submission:
+// const answers = {
+//   1:'I love you',
+//   2:'a little',
+//   3:'a lot',
+//   4:'passionately',
+//   5:'madly',
+//   0:'not at all'
+// };
+
+// const howMuchILoveYou = nbPetals => answers[nbPetals % 6];
