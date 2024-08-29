aac43bc2a3687498adc0750f43b98136193fd830
Andrewf9001 <andrewf9001@gmail.com>
2024-08-29 16:11:27 -0600

    added new submission codewars

--- codewars/fundamentalsTwo/feastOfManyBeasts.js
+++ codewars/fundamentalsTwo/feastOfManyBeasts.js
@@ -29,3 +29,8 @@ const feast = (beast, dish) => {
 // Test.assertEquals(feast("brown bear", "bear claw"), false)
 //   });
 // });
+
+// Submission:
+// function feast(beast, dish) {
+//   return beast[0]===dish[0] && beast.slice(-1)===dish.slice(-1);
+// }
