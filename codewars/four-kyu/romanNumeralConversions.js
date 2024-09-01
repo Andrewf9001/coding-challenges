c407d5348cb5c3e22ebac4cf9502f70cafe977fd
Andrewf9001 <andrewf9001@gmail.com>
2024-09-01 11:11:42 -0600

    cleanedup toRoman method

--- codewars/four-kyu/romanNumeralConversions.js
+++ codewars/four-kyu/romanNumeralConversions.js
@@ -17,14 +17,9 @@ const numerals = {
 class RomanNumerals {
   static toRoman(num) {
     let results = "";
-    const numToString = num.toString();
     const numsArray = Object.entries(numerals);
 
     for (let [key, value] of numsArray) {
-      if (num === 0) {
-        break;
-      }
-
       while (num >= value) {
         results += key;
         num -= value;
