37f299433b6c4f52c7565e3ab8f10f9511d1275e
Andrewf9001 <andrewf9001@gmail.com>
2024-09-22 11:10:03 -0600

    initla attempt whoLikesIt

--- codewars/six-kyu/whoLikesIt.js
+++ codewars/six-kyu/whoLikesIt.js
@@ -1,1 +1,17 @@
-const likes = (names) => {};
+const likes = (names) => {
+  const namesLength = names.length;
+
+  if (namesLength === 0) return "no one likes this";
+  else {
+    if (namesLength === 1) return `${names[0]} likes this`;
+    else if (namesLength === 2) return `${names[0]} and ${names[1]} like this`;
+    else if (namesLength === 3)
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    else {
+      const displayedNames = names.slice(0, 2);
+      const extraUsers = names.length - displayedNames.length;
+
+      return `${displayedNames[0]}, ${displayedNames[1]} and ${extraUsers} others like this`;
+    }
+  }
+};
