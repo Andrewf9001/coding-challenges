582a019c3d09c9b5d5bd1334a40dc4632a0e94a6
Andrewf9001 <andrewf9001@gmail.com>
2024-09-25 14:42:18 -0600

    added dupilicate encoder challenge codewars, reformatted base code, added loop to create object of letter counts

---
+++ codewars/six-kyu/duplicateEncoder.js
@@ -0,0 +1,10 @@
+const duplicateEncode = (word) => {
+  const charCounts = {};
+
+  for (let char of word) {
+    count[char] = (count[char] || 0) + 1;
+  }
+
+  console.log("charCounts", charCounts);
+  return "";
+};
