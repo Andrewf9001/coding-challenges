ffdc923ff5074d18dd38d58443d4968f554dc660
Andrewf9001 <andrewf9001@gmail.com>
2024-09-30 17:44:20 -0600

    opposite number codewars

--- codewars/eight-kyu/oppositeNumber.js
+++ codewars/eight-kyu/oppositeNumber.js
@@ -5,3 +5,42 @@ const opposite = (number) => {
     else return -Math.abs(number);
   }
 };
+
+// Instructions:
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// Tests:
+// const { assert } = require('chai');
+
+// describe('Fixed tests', () => {
+//   it('Is it a function?', () => {
+//     assert.strictEqual(typeof opposite, 'function', 'opposite should be a function');
+//   });
+//   it('should return -1', () => {
+//     assert.strictEqual(opposite(1), -1);
+//   });
+//   it('should return 0', () => {
+//     assert.strictEqual(opposite(0), 0);
+//   });
+//   it('should return -4.25', () => {
+//     assert.strictEqual(opposite(4.25), -4.25);
+//   });
+//   it('should return -3.3333333', () => {
+//     assert.strictEqual(opposite(3.3333333), -3.3333333);
+//   });
+//   it('should return 12525220.3325', () => {
+//     assert.strictEqual(opposite(-12525220.3325), 12525220.3325);
+//   });
+//   it('should return 5', () => {
+//     assert.strictEqual(opposite(-5), 5);
+//   });
+// });
+
+// Submission:
+// const opposite = number => -number;
