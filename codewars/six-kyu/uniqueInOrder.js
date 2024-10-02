2b05db8956193355d45cd002df195eba07873419
Andrewf9001 <andrewf9001@gmail.com>
2024-10-02 15:44:12 -0600

    unique in order codewars

---
+++ codewars/six-kyu/uniqueInOrder.js
@@ -0,0 +1,21 @@
+const uniqueInOrder = (iterable) => {};
+
+// Instructions:
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+// Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.deepEqual(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
+//   });
+// });
