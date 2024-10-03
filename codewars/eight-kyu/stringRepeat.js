4add6e99de9ea292298f12124054b45b126c38be
Andrewf9001 <andrewf9001@gmail.com>
2024-10-03 16:45:09 -0600

    added base for string Repeat codewars

---
+++ codewars/eight-kyu/stringRepeat.js
@@ -0,0 +1,23 @@
+const repeatStr = (n, s) => {
+  return "";
+};
+
+// Instructions:
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", function() {
+//   it ("Basic tests", function() {
+//     assert.strictEqual(repeatStr(3, "*"), "***");
+//     assert.strictEqual(repeatStr(5, "#"), "#####");
+//     assert.strictEqual(repeatStr(2, "ha "), "ha ha ");
+//   });
+// });
