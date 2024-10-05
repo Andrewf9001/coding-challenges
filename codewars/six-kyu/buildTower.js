b47c38fdce3db98f0ce9ae9c2d081d34235ebaa1
Andrewf9001 <andrewf9001@gmail.com>
2024-10-05 14:23:02 -0600

    setup build tower codewars

---
+++ codewars/six-kyu/buildTower.js
@@ -0,0 +1,33 @@
+// Instructions:
+// Build Tower
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ",
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ",
+//   "    ***    ",
+//   "   *****   ",
+//   "  *******  ",
+//   " ********* ",
+//   "***********"
+// ]
+// Go challenge Build Tower Advanced once you have finished this :)
+
+// Tests:
+// const Test = require('@codewars/test-compat');
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertDeepEquals(towerBuilder(1), ["*"]);
+// Test.assertDeepEquals(towerBuilder(2), [" * ","***"]);
+// Test.assertDeepEquals(towerBuilder(3), ["  *  "," *** ","*****"]);
+//   });
+// });
---
+++ codewars/six-kyu/consecutiveStrings.js
@@ -0,0 +1,1 @@
+const longestConsec = (strArr, k) => {};
