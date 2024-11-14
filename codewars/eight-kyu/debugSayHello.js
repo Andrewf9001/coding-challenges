45ee664f70f941d4ac43c6cdf4c0eb117da48051
Andrewf9001 <andrewf9001@gmail.com>
2024-11-14 16:01:08 -0700

    applied final solution

--- codewars/eight-kyu/debugSayHello.js
+++ codewars/eight-kyu/debugSayHello.js
@@ -1,6 +1,4 @@
-const sayHello = (name) => {
-  return "Hello";
-};
+const sayHello = (name) => `Hello, ${name}`;
 
 // Instructions:
 // The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
@@ -8,3 +6,15 @@ const sayHello = (name) => {
 // Example output:
 
 // Hello, Mr. Spock
+
+// Tests:
+// const { assert } = require('chai');
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.strictEqual(sayHello('Mr. Spock'), 'Hello, Mr. Spock')
+//     assert.strictEqual(sayHello('Captain Kirk'), 'Hello, Captain Kirk')
+//     assert.strictEqual(sayHello('Liutenant Uhura'), 'Hello, Liutenant Uhura')
+//     assert.strictEqual(sayHello('Dr. McCoy'), 'Hello, Dr. McCoy')
+//   });
+// });
