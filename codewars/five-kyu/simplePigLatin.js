162208b7ae041328378e6d57a1435acbbe4543a2
Andrewf9001 <andrewf9001@gmail.com>
2025-02-18 14:57:29 -0700

    added instructions, tests, and submission

--- codewars/five-kyu/simplePigLatin.js
+++ codewars/five-kyu/simplePigLatin.js
@@ -19,3 +19,28 @@ const pigIt = (str) => {
 
   return results;
 };
+
+// Instructions
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+// Tests:
+// const Test = require('@codewars/test-compat');
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(pigIt('Pig latin is cool'),'igPay atinlay siay oolcay')
+// Test.assertEquals(pigIt('This is my string'),'hisTay siay ymay tringsay')
+// });
+// });
+
+// Submission
+// function pigIt(str){
+//     return str
+//       .split(" ")
+//       .map(word =>`${ word.substring(1)}${word.split('').shift()}${word.charCodeAt(0) > 63 ? 'ay' : '' }`)
+//       .join(" ");
+//    }
